eae091a7c9cbc078385c550e5a262065f0536065
sosualfred <sosualfred@gmail.com>
2021-05-26 09:43:42 +0000

    implement header element test

---
+++ src/Counter/__test__/Counter.test.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Counter from "../Counter";
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+
+test("header renders with correct text", () => {
+
+    const component = render(<Counter/>);
+
+    const headerEl = component.getByTestId("header");
+
+    expect(headerEl.textContent).toBe('My Counter');
+
+});
