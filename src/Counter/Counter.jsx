814806511b2a0421764f3a269a51febd5c9ece7c
sosualfred <sosualfred@gmail.com>
2021-05-26 14:34:45 +0000

    add additional tests to test functionality

--- src/Counter/Counter.jsx
+++ src/Counter/Counter.jsx
@@ -1,11 +1,47 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-function Counter() {
-    return (
-        <div>
-            <h3 data-testid="header" >My Counter</h3>
-        </div>
-    )
+export class Counter extends Component {
+
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             count: 0,
+             incrementValue: 1
+        }
+    }
+    
+    setIncrementValue = (e) => {
+        this.setState({incrementValue: parseInt(e.target.value)});
+    }
+
+    increaseCounterValue = () => {
+        let newCounterValue = this.state.count + this.state.incrementValue;
+        this.setState({count: newCounterValue});
+    }
+
+    decreaseCounterValue = () => {
+        let newCounterValue = this.state.count - this.state.incrementValue;
+        this.setState({count: newCounterValue});
+    }
+
+    render() {
+        return (
+            <div>
+                <h3 data-testid="header" >My Counter</h3>
+
+                <h2 data-testid="counter">
+                    {this.state.count}
+                </h2>
+
+                <button data-testid="substract-button" onClick={this.decreaseCounterValue}>-</button>
+                <input style={{ textAlign: 'center' }} type="number" data-testid="input" 
+                    value={this.state.incrementValue} onChange={this.setIncrementValue.bind(this)}
+                />
+                <button data-testid="add-button" onClick={this.increaseCounterValue}>+</button>
+            </div>
+        )
+    }
 }
 
-export default Counter;
+export default Counter
--- src/Counter/__test__/Counter.test.js
+++ src/Counter/__test__/Counter.test.js
@@ -1,14 +1,94 @@
 import React from "react";
 import Counter from "../Counter";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 
 test("header renders with correct text", () => {
 
     const component = render(<Counter/>);
-
     const headerEl = component.getByTestId("header");
 
     expect(headerEl.textContent).toBe('My Counter');
+
+});
+
+test("counter initially start with text of 0", () => {
+
+    const component = render(<Counter />);
+    const counterEl = component.getByTestId("counter");
+
+    expect(counterEl.textContent).toBe("0");
+
+});
+
+test("input contains initial value of 1", () => {
+
+    const component = render(<Counter />);
+    const inputEl = component.getByTestId("input");
+
+    expect(inputEl.value).toBe("1");
+
+});
+
+test("add button renders with + sign", () => {
+
+    const component = render(<Counter />);
+    const addButton = component.getByTestId("add-button");
+
+    expect(addButton.textContent).toBe("+");
+
+});
+
+test("substract button renders with - sign", () => {
+
+    const component = render(<Counter />);
+    const substractButton = component.getByTestId("substract-button");
+
+    expect(substractButton.textContent).toBe("-");
+
+});
+
+test("change value of input works correctly", () => {
+
+    const component = render(<Counter />);
+    const inputEl = component.getByTestId("input");
+
+    expect(inputEl.value).toBe("1");
+
+    fireEvent.change(inputEl, {
+        target: {
+            value: "5"
+        }
+    });
+
+    expect(inputEl.value).toBe("5");
+
+});
+
+test("clicking on add button adds 1 to counter", () => {
+
+    const component = render(<Counter />);
+    const addButton = component.getByTestId("add-button");
+    const counterEl = component.getByTestId("counter");
+
+    expect(counterEl.textContent).toBe("0");
+
+    fireEvent.click(addButton);
+
+    expect(counterEl.textContent).toBe("1");
+
+});
+
+test("clicking on substract button substracts 1 from the counter", () => {
+
+    const component = render(<Counter />);
+    const substractButton = component.getByTestId("substract-button");
+    const counterEl = component.getByTestId("counter");
+
+    expect(counterEl.textContent).toBe("0");
+
+    fireEvent.click(substractButton);
+
+    expect(counterEl.textContent).toBe("-1");
 
 });
